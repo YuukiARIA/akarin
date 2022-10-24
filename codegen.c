72f10848665715b833063ea3e3f94f07f9de34d6
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2022-10-25 07:53:55 +0900

    Implement arithmetic

--- codegen.c
+++ codegen.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include "codegen.h"
 #include "node.h"
+#include "operator.h"
 
 struct codegen_t {
   node_t *root;
 };
 
+static void gen(node_t *node);
+static void gen_assign(node_t *node);
+static void gen_arith(node_t *node);
 static void encode_integer(int n);
 static void encode_uint_rec(unsigned int n);
 
@@ -22,6 +26,56 @@ void codegen_release(codegen_t **pcodegen) {
 }
 
 void codegen_generate(codegen_t *codegen) {
+  gen(codegen->root);
+  puts("LLL");
+}
+
+static void gen(node_t *node) {
+  switch (node_get_ntype(node)) {
+  case NT_BINARY:
+    if (node_get_bop(node) == BOP_ASSIGN) {
+      gen_assign(node);
+    }
+    else {
+      gen_arith(node);
+    }
+    break;
+  case NT_INTEGER:
+    printf("SS");
+    encode_integer(node_get_value(node));
+    break;
+  default:
+    break;
+  }
+}
+
+static void gen_assign(node_t *node) {
+  gen(node_get_r(node));
+}
+
+static void gen_arith(node_t *node) {
+  gen(node_get_l(node));
+  gen(node_get_r(node));
+
+  switch (node_get_bop(node)) {
+  case BOP_ADD:
+    printf("TSSS");
+    break;
+  case BOP_SUB:
+    printf("TSST");
+    break;
+  case BOP_MUL:
+    printf("TSSL");
+    break;
+  case BOP_DIV:
+    printf("TSTS");
+    break;
+  case BOP_MOD:
+    printf("TSTT");
+    break;
+  default:
+    break;
+  }
 }
 
 static void encode_integer(int n) {
