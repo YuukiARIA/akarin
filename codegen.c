f5cb48d0bcceb9039821f4603ec47d6da069db23
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2022-10-25 21:38:53 +0900

    Implement if-else statement

--- codegen.c
+++ codegen.c
@@ -6,19 +6,24 @@
 
 struct codegen_t {
   node_t *root;
+  int     label_count;
 };
 
-static void gen(node_t *node);
-static void gen_if_statement(node_t *node);
-static void gen_unary(node_t *node);
-static void gen_assign(node_t *node);
-static void gen_arith(node_t *node);
+static void gen(codegen_t *codegen, node_t *node);
+static void gen_if_statement(codegen_t *codegen, node_t *node);
+static void gen_unary(codegen_t *codegen, node_t *node);
+static void gen_assign(codegen_t *codegen, node_t *node);
+static void gen_arith(codegen_t *codegen, node_t *node);
 static void encode_integer(int n);
+static void encode_uint(unsigned int n);
 static void encode_uint_rec(unsigned int n);
+static int  alloc_label_id(codegen_t *codegen);
+static void gen_label(int label_id);
 
 codegen_t *codegen_new(node_t *root) {
   codegen_t *codegen = (codegen_t *)malloc(sizeof(codegen_t));
   codegen->root = root;
+  codegen->label_count = 0;
   return codegen;
 }
 
@@ -28,36 +33,36 @@ void codegen_release(codegen_t **pcodegen) {
 }
 
 void codegen_generate(codegen_t *codegen) {
-  gen(codegen->root);
+  gen(codegen, codegen->root);
   puts("LLL");
 }
 
-static void gen(node_t *node) {
+static void gen(codegen_t *codegen, node_t *node) {
   switch (node_get_ntype(node)) {
   case NT_SEQ:
-    gen(node_get_l(node));
-    gen(node_get_r(node));
+    gen(codegen, node_get_l(node));
+    gen(codegen, node_get_r(node));
     break;
   case NT_IF:
-    gen_if_statement(node);
+    gen_if_statement(codegen, node);
     break;
   case NT_PUTI:
-    gen(node_get_l(node));
+    gen(codegen, node_get_l(node));
     printf("TLST");
     break;
   case NT_PUTC:
-    gen(node_get_l(node));
+    gen(codegen, node_get_l(node));
     printf("TLSS");
     break;
   case NT_UNARY:
-    gen_unary(node);
+    gen_unary(codegen, node);
     break;
   case NT_BINARY:
     if (node_get_bop(node) == BOP_ASSIGN) {
-      gen_assign(node);
+      gen_assign(codegen, node);
     }
     else {
-      gen_arith(node);
+      gen_arith(codegen, node);
     }
     break;
   case NT_INTEGER:
@@ -69,32 +74,39 @@ static void gen(node_t *node) {
   }
 }
 
-static void gen_if_statement(node_t *node) {
+static void gen_if_statement(codegen_t *codegen, node_t *node) {
   node_t *cond = node_get_cond(node), *then = node_get_l(node), *els = node_get_r(node);
+  int l1 = alloc_label_id(codegen), l2 = alloc_label_id(codegen);
 
-  gen(cond);
+  gen(codegen, cond);
 
   /* IF FALSE GOTO L1 */
+  printf("LTS");
+  encode_uint((unsigned int)l1);
 
-  gen(then);
+  gen(codegen, then);
 
   /* GOTO L2 */
+  printf("LSL");
+  encode_uint((unsigned int)l2);
 
   /* L1 */
+  gen_label(l1);
 
   if (els) {
-    gen(els);
+    gen(codegen, els);
   }
 
   /* L2 */
+  gen_label(l2);
 }
 
-static void gen_unary(node_t *node) {
+static void gen_unary(codegen_t *codegen, node_t *node) {
   switch (node_get_uop(node)) {
   case UOP_NEGATIVE: /* implement -x as 0 - x. */
     printf("SS");
     encode_integer(0);
-    gen(node_get_l(node));
+    gen(codegen, node_get_l(node));
     printf("TSST");
     break;
   default:
@@ -102,13 +114,13 @@ static void gen_unary(node_t *node) {
   }
 }
 
-static void gen_assign(node_t *node) {
-  gen(node_get_r(node));
+static void gen_assign(codegen_t *codegen, node_t *node) {
+  gen(codegen, node_get_r(node));
 }
 
-static void gen_arith(node_t *node) {
-  gen(node_get_l(node));
-  gen(node_get_r(node));
+static void gen_arith(codegen_t *codegen, node_t *node) {
+  gen(codegen, node_get_l(node));
+  gen(codegen, node_get_r(node));
 
   switch (node_get_bop(node)) {
   case BOP_ADD:
@@ -137,9 +149,28 @@ static void encode_integer(int n) {
   putchar('L');
 }
 
+static void encode_uint(unsigned int n) {
+  if (n == 0) {
+    putchar('S');
+  }
+  else {
+    encode_uint_rec(n);
+  }
+  putchar('L');
+}
+
 static void encode_uint_rec(unsigned int n) {
   if (n != 0) {
     encode_uint_rec(n >> 1);
     putchar((n & 1) ? 'T' : 'S');
   }
+}
+
+static int alloc_label_id(codegen_t *codegen) {
+  return codegen->label_count++;
+}
+
+static void gen_label(int label_id) {
+  printf("LSS");
+  encode_uint((unsigned int)label_id);
 }
