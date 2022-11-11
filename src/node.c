4b867f56dc82d697560c53d4888f3cfae90e3e0d
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2022-11-11 22:10:52 +0900

    Improve pretty printing syntax tree

--- src/node.c
+++ src/node.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -301,164 +302,166 @@ bool node_is_all_paths_ended_with_return(node_t *node) {
   }
 }
 
-static void print_indent(int indent) {
-  int i;
-
+static void print_indent(int indent, uint64_t mask) {
   if (indent == 0) {
     return;
   }
 
-  for (i = 0; i < indent - 1; ++i) {
-    putchar('|');
+  for (int i = 0; i < indent - 1; ++i) {
+    bool b = (mask >> (indent - 1 - i)) & 1;
+    putchar(b ? '|' : ' ');
     putchar(' ');
   }
   putchar('+');
   putchar('-');
 }
 
-static void indent_puts(int indent, const char *s) {
-  print_indent(indent);
+static void indent_puts(int indent, uint64_t mask, const char *s) {
+  print_indent(indent, mask);
   puts(s);
 }
 
-static void indent_printf(int indent, const char *fmt, ...) {
-  print_indent(indent);
+static void indent_printf(int indent, uint64_t mask, const char *fmt, ...) {
+  print_indent(indent, mask);
   va_list args;
   va_start(args, fmt);
   vprintf(fmt, args);
   va_end(args);
 }
 
-static void dump_rec(node_t *node, int indent) {
+static void dump_rec(node_t *node, int indent, uint64_t mask);
+
+static void dump_children(node_t *node, int indent, uint64_t mask) {
+  for (int i = 0; i < node->children_count; ++i) {
+    bool is_last = i == node->children_count - 1;
+    dump_rec(node->children[i], indent, mask | !is_last);
+  }
+}
+
+static void dump_rec(node_t *node, int indent, uint64_t mask) {
+  uint64_t mask0 = mask << 1;
+  uint64_t mask1 = mask0 | 1;
+
   switch (node->ntype) {
   case NT_INVALID:
-    indent_puts(indent, "Invalid");
+    indent_puts(indent, mask, "Invalid");
     break;
   case NT_GROUP:
-    indent_puts(indent, node->name);
-    dump_rec(node->children[0], indent + 1);
+    indent_puts(indent, mask, node->name);
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_EMPTY:
     break;
   case NT_SEQ:
-    for (int i = 0; i < node->children_count; ++i) {
-      dump_rec(node->children[i], indent);
-    }
+    dump_children(node, indent, mask);
     break;
   case NT_EXPR:
-    indent_puts(indent, "Expr");
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "Expr");
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_UNARY:
-    indent_printf(indent, "Unary %s\n", unary_op_to_string(node->uop));
-    dump_rec(node->l, indent + 1);
+    indent_printf(indent, mask, "Unary %s\n", unary_op_to_string(node->uop));
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_BINARY:
-    indent_printf(indent, "Binary %s\n", binary_op_to_string(node->bop));
-    dump_rec(node->l, indent + 1);
-    dump_rec(node->r, indent + 1);
+    indent_printf(indent, mask, "Binary %s\n", binary_op_to_string(node->bop));
+    dump_rec(node->l, indent + 1, mask1);
+    dump_rec(node->r, indent + 1, mask0);
     break;
   case NT_ASSIGN:
-    indent_puts(indent, "Assign");
-    dump_rec(node->l, indent + 1);
-    dump_rec(node->r, indent + 1);
+    indent_puts(indent, mask, "Assign");
+    dump_rec(node->l, indent + 1, mask1);
+    dump_rec(node->r, indent + 1, mask0);
+    break;
   case NT_INTEGER:
-    indent_printf(indent, "Integer %d\n", node->value);
+    indent_printf(indent, mask, "Integer %d\n", node->value);
     break;
   case NT_IDENT:
-    indent_printf(indent, "Ident %s\n", node->name);
+    indent_printf(indent, mask, "Ident %s\n", node->name);
     break;
   case NT_VARIABLE:
-    indent_puts(indent, "Variable");
-    dump_rec(node->children[0], indent + 1);
+    indent_puts(indent, mask, "Variable");
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_ARRAY:
-    indent_puts(indent, "Array");
-    dump_rec(node->l, indent + 1);
-    dump_rec(node->r, indent + 1);
+    indent_puts(indent, mask, "Array");
+    dump_rec(node->l, indent + 1, mask1);
+    dump_rec(node->r, indent + 1, mask0);
     break;
   case NT_FUNC_CALL:
-    indent_puts(indent, "FuncCall");
-    dump_rec(node->children[0], indent + 1);
-    dump_rec(node->children[1], indent + 1);
+    indent_puts(indent, mask, "FuncCall");
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_FUNC_CALL_ARG:
-    indent_puts(indent, "FuncCallArg");
-    for (int i = 0; i < node->children_count; ++i) {
-      dump_rec(node->children[i], indent + 1);
-    }
+    indent_puts(indent, mask, "FuncCallArg");
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_IF:
-    indent_puts(indent, "If-Statement");
-    dump_rec(node->cond, indent + 1);
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "If-Statement");
+    dump_rec(node->cond, indent + 1, mask1);
+    dump_rec(node->l, indent + 1, node->r ? mask1 : mask0);
     if (node->r) {
-      dump_rec(node->r, indent + 1);
+      dump_rec(node->r, indent + 1, mask0);
     }
     break;
   case NT_WHILE:
-    indent_puts(indent, "While-Statement");
-    dump_rec(node->cond, indent + 1);
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "While-Statement");
+    dump_rec(node->cond, indent + 1, mask1);
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_LOOP_STATEMENT:
-    indent_puts(indent, "Loop-Statement");
-    dump_rec(node->children[0], indent + 1);
+    indent_puts(indent, mask, "Loop-Statement");
+    dump_rec(node->children[0], indent + 1, mask0);
     break;
   case NT_BREAK:
-    indent_puts(indent, "Break-Statement");
+    indent_puts(indent, mask, "Break-Statement");
     break;
   case NT_CONTINUE:
-    indent_puts(indent, "Continue-Statement");
+    indent_puts(indent, mask, "Continue-Statement");
     break;
   case NT_PUTI:
-    indent_puts(indent, "Puti-Statement");
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "Puti-Statement");
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_PUTC:
-    indent_puts(indent, "Putc-Statement");
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "Putc-Statement");
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_GETI:
-    indent_puts(indent, "Geti-Statement");
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "Geti-Statement");
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_GETC:
-    indent_puts(indent, "Getc-Statement");
-    dump_rec(node->l, indent + 1);
+    indent_puts(indent, mask, "Getc-Statement");
+    dump_rec(node->l, indent + 1, mask0);
     break;
   case NT_ARRAY_DECL:
-    indent_puts(indent, "ArrayDecl-Statement");
-    dump_rec(node->l, indent + 1);
-    dump_rec(node->r, indent + 1);
+    indent_puts(indent, mask, "ArrayDecl-Statement");
+    dump_rec(node->l, indent + 1, mask1);
+    dump_rec(node->r, indent + 1, mask0);
     break;
   case NT_RETURN:
-    indent_puts(indent, "Return");
-    dump_rec(node->children[0], indent + 1);
+    indent_puts(indent, mask, "Return");
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_HALT:
-    indent_puts(indent, "Halt-Statement");
+    indent_puts(indent, mask, "Halt-Statement");
     break;
   case NT_FUNC:
-    indent_puts(indent, "Func");
-    dump_rec(node->children[0], indent + 1);
-    dump_rec(node->children[1], indent + 1);
-    dump_rec(node->children[2], indent + 1);
+    indent_puts(indent, mask, "Func");
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_FUNC_PARAM:
-    indent_puts(indent, "FuncParam");
-    for (int i = 0; i < node->children_count; ++i) {
-      dump_rec(node->children[i], indent + 1);
-    }
+    indent_puts(indent, mask, "FuncParam");
+    dump_children(node, indent + 1, mask0);
     break;
   case NT_CONST_STATEMENT:
-    indent_puts(indent, "Const-Statement");
-    dump_rec(node->children[0], indent + 1);
-    dump_rec(node->children[1], indent + 1);
+    indent_puts(indent, mask, "Const-Statement");
+    dump_children(node, indent + 1, mask0);
     break;
   }
 }
 
 void node_dump_tree(node_t *node) {
-  dump_rec(node, 0);
+  dump_rec(node, 0, 0);
 }
